7167cbd002b5bcfd8559bbe8c035cbacf2f01116
Ryo Chiba <rchiba8030@gmail.com>
2022-06-10 09:35:28 +0900

    3rd

--- model/db.go
+++ model/db.go
@@ -13,12 +13,19 @@ var DB *gorm.DB
 var err error
 
 func init() {
-	// connect DB
+	// Connect to DB
 	conf := config.Config
 	dsn := conf.DbUser + ":" + conf.DbPassword + "@tcp(" + conf.DbHost + ":" + conf.DbPort + ")/" + conf.DbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// Open DB
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(dsn + "database can't connect")
 	}
+
+	// 
+	// DB.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
+
+	// Make migration
 	DB.AutoMigrate(&Member{})
 }
--- model/member.go
+++ model/member.go
@@ -1,19 +1,38 @@
 package model
 
 import (
-//	"time"
+	"time"
 	"gorm.io/gorm"
+	"github.com/google/uuid"
+	"fmt"
 )
 
+type UUIDBaseModel struct {
+	ID uuid.UUID `gorm:"primaryKey;unique;type:uuid"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `"gorm:index"`
+}
+
+// This functions are called before creating Member
+func (uuidbasemodel *UUIDBaseModel) BeforeCreate (tx *gorm.DB) error {
+	if uuid, err := uuid.NewRandom(); err != nil {
+		fmt.Println(err)
+		return err
+	} else {
+		tx.Statement.SetColumn("ID", uuid.String())
+	}
+
+	return nil
+}
+
 type Member struct {
-//	gorm.Model
-	ID		uint		`json:"id" gorm:"auto_increment;primary_key"`
+	UUIDBaseModel
+	//	ID		uint		`json:"id" gorm:"auto_increment;primary_key"`
 	Name	string	`json:"name" gorm:"type:varchar(255);not null"`
 	Age		uint		`json:"age" gorm:"not null;default:0"`
 	Bloodtype	string	`json:"bloodtype" gorm:"type:varchar(255);not null"`
 	Origin	string	`json:"origin" gorm:"type:varchar(255);not null"`
-//	CreatedAt	time.Time
-//	UpdatedAt	time.Time// `json:"updated_at"`
 }
 
 func (p *Member) FirstById(id uint) (tx *gorm.DB) {
